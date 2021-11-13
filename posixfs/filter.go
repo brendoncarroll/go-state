a391b6bbfefc644bffe22060e146d21fe647e02b
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-11-13 10:46:57 -0500

    filter directory entries in posixfs.Filtered

--- posixfs/filter.go
+++ posixfs/filter.go
@@ -1,6 +1,10 @@
 package posixfs
 
-import "github.com/pkg/errors"
+import (
+	"path"
+
+	"github.com/pkg/errors"
+)
 
 type filtered struct {
 	x         FS
@@ -19,7 +23,17 @@ func (fs filtered) OpenFile(p string, flag int, perm FileMode) (File, error) {
 	if err := fs.checkPath(p); err != nil {
 		return nil, err
 	}
-	return fs.x.OpenFile(p, flag, perm)
+	f, err := fs.x.OpenFile(p, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+	ff := filteredFile{
+		File: f,
+		namePred: func(name string) bool {
+			return fs.predicate(path.Join(p, name))
+		},
+	}
+	return &ff, nil
 }
 
 func (fs filtered) Mkdir(p string, perm FileMode) error {
@@ -64,5 +78,26 @@ func (fs filtered) checkPath(p string) error {
 	if fs.predicate(p) {
 		return nil
 	}
-	return errors.Errorf("path %s has been filtered", p)
+	return errors.Errorf("path %q has been filtered", p)
+}
+
+type filteredFile struct {
+	File
+	namePred func(string) bool
+}
+
+func (ff *filteredFile) ReadDir(n int) ([]DirEnt, error) {
+	dirEnts, err := ff.File.ReadDir(n)
+	if err != nil {
+		return nil, err
+	}
+	var deleted int
+	for i := range dirEnts {
+		if ff.namePred(dirEnts[i].Name) {
+			dirEnts[i-deleted] = dirEnts[i]
+		} else {
+			deleted++
+		}
+	}
+	return dirEnts[:len(dirEnts)-deleted], nil
 }
